9891f5a1eef78b10342fdace1a61dbc196930f0e
Sanjee <sanjee.yogeswaran@gmail.com>
2017-07-19 20:50:14 -0500

    updated screenshot

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 date=$(date +%m-%d-%Y.%H%M) 
+cd /home/heroinhero/rnba
 wkhtmltoimage -f png --width 1920 http://www.reddit.com/r/nba screenshots/${date}.png
 git add .
 git commit -m "updated screenshot"
